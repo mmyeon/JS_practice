2f0a3919c814319af78bc87f6b7a0149160f72f8
mmyeon <mindfulyeon@gmail.com>
2020-11-30 18:15:34 +0900

    [Refactoring] 코드 중복 개선

--- countdown-timer2/index.js
+++ countdown-timer2/index.js
@@ -2,20 +2,26 @@ const clock = document.querySelector(".js-clock");
 
 function getTime() {
   const xmasDay = new Date("2020-12-24:00:00:00+0900");
+  // miliseconds를 리턴함
+  // 1초 = 1000ms
   const now = new Date();
-  const totalSeconds = (xmasDay - now) / 1000;
+  const difference = new Date(xmasDay - now);
+  const secondsInMs = Math.floor(difference / 1000);
+  const minutesInMs = Math.floor(secondsInMs / 60);
+  const hoursInMs = Math.floor(minutesInMs / 60);
 
-  const days = Math.floor(totalSeconds / (60 * 60 * 24));
-  const hours = Math.floor((totalSeconds / (60 * 60)) % 24);
-  const minutes = Math.floor((totalSeconds / 60) % 60);
-  const seconds = Math.floor(totalSeconds) % 60;
+  const days = Math.floor(hoursInMs / 24);
+  const hours = Math.floor(hoursInMs % 24);
+  const minutes = Math.floor(minutesInMs % 60);
+  const seconds = Math.floor(secondsInMs % 60);
 
-  const daysString = `${days < 10 ? `0${days}d` : `${days}d`}`;
-  const hoursString = `${hours < 10 ? `0${hours}d` : `${hours}d`}`;
-  const minutesString = `${minutes < 10 ? `0${minutes}d` : `${minutes}d`}`;
-  const secondsString = `${seconds < 10 ? `0${seconds}d` : `${seconds}d`}`;
+  // 중복 피하기
+  const daysStr = `${days < 10 ? 0`${days}` : days}d`;
+  const hoursStr = `${hours < 10 ? `0${hours}` : hours}h`;
+  const minutesStr = `${minutes < 10 ? `0${minutes}` : minutes}m`;
+  const secondsStr = `${seconds < 10 ? `0${seconds}` : seconds}d`;
 
-  clock.innerText = `${daysString} ${hoursString} ${minutesString} ${secondsString}`;
+  clock.innerText = `${daysStr} ${hoursStr} ${minutesStr} ${secondsStr}`;
 }
 
 function init() {
