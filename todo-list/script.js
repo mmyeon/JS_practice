9d917775cc988e7278f71322cc79a0834f45cd32
mmyeon <mindfulyeon@gmail.com>
2020-12-03 17:30:49 +0900

    [Todolist] 새로운 task 추가 > 추가된 task를 표시

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,5 +1,6 @@
-const toDoForm = document.querySelector(".js-toDoForm"),
+const toDoForm = document.querySelector("#js-toDoForm"),
   input = toDoForm.querySelector("input");
+pendingListDom = document.querySelector("#js-pendingList");
 
 const PENDING = "pending";
 
@@ -21,10 +22,26 @@ function handleSubmit(e) {
 
 function saveToStorage(toDos) {
   localStorage.setItem(PENDING, JSON.stringify(toDos));
+  displayTaskToPendingList();
 }
 
 function init() {
   toDoForm.addEventListener("submit", handleSubmit);
+}
+
+function displayTaskToPendingList() {
+  const pendingList = JSON.parse(localStorage.getItem(PENDING));
+
+  pendingListDom.innerHTML = "";
+
+  pendingList.forEach((v) => {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = v.text;
+    pendingListDom.appendChild(li);
+    li.appendChild(span);
+    li.id = v.id;
+  });
 }
 
 init();
